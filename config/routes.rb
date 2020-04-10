691d057735f1624d6f13c176e6a5828517c5b1f3
vagrant <vagrant@localhost.localdomain>
2020-04-10 09:52:19 +0000

    make top-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-
 	devise_for :users
+
 	root 'books#top'
 	resources :books, only: [:create, :index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
