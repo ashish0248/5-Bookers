4149bf2036bd6dd79ba580232c34b1459571efe5
Your Name <hajime@hajimenoMacBook-Pro.local>
2020-04-10 15:07:00 +0900

    make books-controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,13 @@
 class BooksController < ApplicationController
-  def index
-  end
 
-  def top
-  end
+	def top
+  	end
 
-  def show
-  end
+	def index
+	end
+
+
+	def show
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/top'
-  get 'books/show'
-  devise_for :users
+
+
+	devise_for :users
+	root 'books#top'
+	resources :books, only: [:create, :index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
