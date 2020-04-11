e5f35ad87d463b58c01954019aed2bdab52b6ccd
vagrant <vagrant@localhost.localdomain>
2020-04-11 06:56:44 +0000

    make users-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
 	devise_for :users
 
 	root 'books#top'
-	resources :books, only: [:create, :index, :show]
+	resources :books, only: [:create, :index, :show, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
