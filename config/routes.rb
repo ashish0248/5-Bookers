158d59117572356b04787719b29eb85b7c9fe861
vagrant <vagrant@localhost.localdomain>
2020-04-12 02:07:57 +0000

    add function

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
 	devise_for :users
 
 	root 'books#top'
+	get 'home/about' => 'books#new'
 
-	# resources :books, only: [:create, :index, :show, :edit, :update]
-	resources :books
+	resources :books, only: [:create, :index, :show, :edit, :update, :delete]
 	resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
